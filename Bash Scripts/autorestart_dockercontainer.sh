5acc4e227f7baac20c2287468322ecba93959567
Kevin <email4kevin91@gmail.com>
2023-04-23 21:56:34 -0400

    Auto-restart arrs and qbittorrent

--- Bash Scripts/autorestart_dockercontainer.sh
+++ Bash Scripts/autorestart_dockercontainer.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 /usr/bin/docker container restart qbittorrent
+/usr/bin/docker container restart overseerr
+/usr/bin/docker container restart prowlarr
+/usr/bin/docker container restart lidarr
+/usr/bin/docker container restart radarr
+/usr/bin/docker container restart sonarr
+/usr/bin/docker container restart bazarr
